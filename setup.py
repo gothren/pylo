05a6d8a0a1e4737473f45f5162b34c6a39276bcb
Chris Huszcza <chris.huszcza@snyk.io>
2020-11-11 21:31:02 +0000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pylo',
+      version='0.0.1',
+      description='Local task execution lib',
+      packages=['pylo'],
+     )
